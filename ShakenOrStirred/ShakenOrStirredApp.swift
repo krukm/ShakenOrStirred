025abc83f29cc7524e0c86fa137a330a33fab89f
krukm <matthew.kruk@gmail.com>
2021-01-20 10:42:11 -0500

    Initial Commit

---
+++ ShakenOrStirred/ShakenOrStirredApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakenOrStirredApp.swift
+//  ShakenOrStirred
+//
+//  Created by Matthew Kruk on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ShakenOrStirredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
